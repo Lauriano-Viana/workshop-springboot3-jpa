867e63acf98817818d414dd70402a6129c5252de
Lauriano Viana <laurianoviana12@gmail.com>
2024-03-26 16:15:07 -0300

    Payment, one-to-one association

--- src/main/java/com/lauriscode/curso/config/TestConfig.java
+++ src/main/java/com/lauriscode/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lauriscode.curso.entities.Category;
 import com.lauriscode.curso.entities.Order;
 import com.lauriscode.curso.entities.OrderItem;
+import com.lauriscode.curso.entities.Payment;
 import com.lauriscode.curso.entities.Product;
 import com.lauriscode.curso.entities.User;
 import com.lauriscode.curso.entities.enums.OrderStatus;
@@ -77,6 +78,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1 );
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
